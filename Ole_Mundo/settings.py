c7e89f63867889db090936a63f1337c1c0bbb86e
lane <ltdouthit@email.arizona.edu>
2018-02-27 21:10:40 -0700

    Touch up Commit

--- Ole_Mundo/settings.py
+++ Ole_Mundo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")#'yj@0x5g2u-kxse3t9fgq^6=5s_pjc2t4h4jjxb%jgc+=x+nanr'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
