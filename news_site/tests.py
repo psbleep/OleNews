e952fab5f4e26c9850c23e22dde66cbc5d474ae3
lane <ltdouthit@email.arizona.edu>
2018-03-01 13:27:03 -0700

    Played around with tests and added def to views and added include to urls

--- news_site/tests.py
+++ news_site/tests.py
@@ -7,29 +7,35 @@ class NewsPostTestCase(TestCase):
     def create_news_post(self, title='Hello World', slug='hello-world',
                          content='Hello World, what more is there to say?',
                          author='Test Author'):
+        print("Creating Test NewsPost")
         return NewsPost(title=title, slug=slug, content=content, author=author)
 
     def test_news_post_creation(self):
         news_post = self.create_news_post()
+        print("Asserting Title")
         self.assertEqual(news_post.title, 'Hello World')
 
     def test_save_slugify_when_no_slug(self):
         news_post = self.create_news_post(slug=None)
         news_post.save()
+        print("Asserting Slug")
         self.assertEqual(news_post.slug, 'hello-world')
 
     def test_save_retains_values(self):
         news_post = self.create_news_post()
         news_post.content = 'Changed Content'
         news_post.save()
+        print("Asserting Changed Content")
         self.assertEqual(news_post.content, 'Changed Content')
 
     def test_get_absolute_url(self):
         news_post = self.create_news_post()
+        print("Asserting URL")
         self.assertEqual(news_post.get_absolute_url(),
                          '/news_post_detail/hello-world/')
 
     def test_get_absolute_url_no_slug(self):
         news_post = self.create_news_post(slug=None)
+        print("Asserting Slug Constructor")
         self.assertEqual(news_post.get_absolute_url(),
                          '/news_post_detail/None/')
--- news_site/views.py
+++ news_site/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def author_about(request, name):
+    print(name)
+    return HttpResponse("Just A temp string!")
+'''
+To Do: Find better naming conventions set up Author login and have it generate a
+test for this URL convention
+'''
